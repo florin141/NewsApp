1b52b28220b12e18df7c4521dbfe0b8cc7c8cf4d
Florin CIOBANU <29834996+florin141@users.noreply.github.com>
2020-09-07 00:16:00 +0200

    Add sample EditTextPreference

--- app/src/main/java/com/example/android/newsapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.newsapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -15,5 +16,10 @@ public class SettingsActivity extends AppCompatActivity {
 
     public static class NewsPreferenceFragment extends PreferenceFragment {
 
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 }
